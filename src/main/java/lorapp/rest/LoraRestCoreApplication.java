8ebe61a49606f92c9139d8c7d848168bc69e97d8
Tristan Yu <magix.yu@gmail.com>
2016-12-05 23:18:46 +0800

    Add Swagger2 document

--- src/main/java/lorapp/rest/LoraRestCoreApplication.java
+++ src/main/java/lorapp/rest/LoraRestCoreApplication.java
@@ -1,8 +1,7 @@
-package lorapp.nslistener;
+package lorapp.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 @SpringBootApplication
---
+++ src/main/java/lorapp/rest/controller/RawdataController.java
@@ -0,0 +1,57 @@
+package lorapp.rest.controller;
+
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import lorapp.db.entity.UploadMessage;
+import lorapp.db.entity.component.AppDevMap;
+import lorapp.db.repo.UploadMessageRepo;
+
+@RestController
+@RequestMapping("/rawdata/")
+public class RawdataController {
+	
+	@Autowired
+	UploadMessageRepo umRepo;
+	
+	@ApiOperation(value="List all data for a AppEUI and DevEUI Map", notes="")
+    @ApiResponse(code = 200, message = "Success", response = List.class)
+	@RequestMapping(value = "{appeui}/{deveui}/all", method = RequestMethod.GET)
+	public List<UploadMessage> getAllData(@PathVariable("appeui") String appEUI,
+			@PathVariable("deveui") String devEUI) {
+		return umRepo.findAllByDevAppMap(new AppDevMap(appEUI, devEUI));
+	}
+	
+	@RequestMapping(value = "{appeui}/{deveui}/day", method = RequestMethod.GET)
+	public List<UploadMessage> getAllDataForDay(@PathVariable("appeui") String appEUI,
+			@PathVariable("deveui") String devEUI,
+			@RequestParam("date") String day) {
+		LocalDate localDate = LocalDate.parse(day);
+		LocalDate nextday = localDate.plusDays(1);
+		return umRepo.findAllByDevAppMapAndReceiveTimeBetween(
+				new AppDevMap(appEUI, devEUI), 
+				Date.from(localDate.atStartOfDay(ZoneId.systemDefault()).toInstant()),
+				Date.from(nextday.atStartOfDay(ZoneId.systemDefault()).toInstant())
+				);
+	}
+	
+	@RequestMapping(value = "{appeui}/{deveui}", method = RequestMethod.GET)
+	public List<UploadMessage> getAllDataBetween(@PathVariable("appeui") String appEUI,
+			@PathVariable("deveui") String devEUI,
+			@RequestParam("from") @DateTimeFormat(pattern="yyyy-MM-dd") Date from,
+			@RequestParam("to")   @DateTimeFormat(pattern="yyyy-MM-dd") Date to) {
+		return umRepo.findAllByDevAppMapAndReceiveTimeBetween(new AppDevMap(appEUI, devEUI), from, to);
+	}
+}
