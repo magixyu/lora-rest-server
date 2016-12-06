a8bd365d6b62aff4c6db4769054bcdb203ad5c87
Tristan Yu <Tristan.Yu@alcatel-lucent.com>
2016-12-06 16:16:22 +0800

    Change the Application package

--- src/main/java/lorapp/rest/LoraRestCoreApplication.java
+++ src/main/java/lorapp/rest/LoraRestCoreApplication.java
@@ -1,9 +1,13 @@
-package lorapp;
+package lorapp.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages="lorapp.db.repo")
+@EntityScan(basePackages="lorapp.db.entity")
 public class LoraRestCoreApplication {
 
 	public static void main(String[] args) {
--- src/main/java/lorapp/rest/Swagger2Config.java
+++ src/main/java/lorapp/rest/Swagger2Config.java
@@ -1,4 +1,4 @@
-package lorapp;
+package lorapp.rest;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
