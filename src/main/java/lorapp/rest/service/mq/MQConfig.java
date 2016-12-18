2cdc415e3041f2ad7e0b3357d4f33a990963baf5
Tristan Yu <Tristan.Yu@alcatel-lucent.com>
2016-12-18 20:46:43 +0800

    Add Bean for rabbitAdmin

--- src/main/java/lorapp/rest/service/mq/MQConfig.java
+++ src/main/java/lorapp/rest/service/mq/MQConfig.java
@@ -12,6 +12,7 @@ import org.springframework.amqp.rabbit.listener.MessageListenerContainer;
 import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
 import org.springframework.stereotype.Component;
@@ -27,126 +28,128 @@ import java.util.concurrent.ConcurrentHashMap;
  */
 @Configuration
 @Component(value = "mqConfig")
-public class MQConfig{
-    @Value("${spring.rabbitmq.host}")
-    private String host;
+public class MQConfig {
+	@Value("${spring.rabbitmq.host}")
+	private String host;
 
-    @Value("${spring.rabbitmq.username}")
-    private String userName;
+	@Value("${spring.rabbitmq.username}")
+	private String userName;
 
-    @Value("${spring.rabbitmq.password}")
-    private String passwd;
+	@Value("${spring.rabbitmq.password}")
+	private String passwd;
 
-    @Value("${spring.rabbitmq.virtual-host}")
-    private String virtualHost;
+	@Value("${spring.rabbitmq.virtual-host}")
+	private String virtualHost;
 
-    @Value("${lora.rawdata.exchange}")
-    private String exch;
+	@Value("${lora.rawdata.exchange}")
+	private String exch;
 
-    @Autowired
-    RabbitAdmin rabbitAdmin;
-    @Autowired
-    ConnectionFactory connectionFactory;
-/*    private ConnectionFactory connectionFactory;
-    private RabbitAdmin rabbitAdmin;*/
+	@Autowired
+	RabbitAdmin rabbitAdmin;
+	@Autowired
+	ConnectionFactory connectionFactory;
 
-    private Map<String, TopicExchange> topicExchangeMap = new ConcurrentHashMap<>();
-    private Map<String, Queue> queueMap = new ConcurrentHashMap<>();
-    private Map<String, AbstractMessageListenerContainer> queueListenerMap = new HashMap<>();
+	/*
+	 * private ConnectionFactory connectionFactory; private RabbitAdmin
+	 * rabbitAdmin;
+	 */
 
-    public MQConfig(){
-/*        CachingConnectionFactory cachingConnectionFactory = new CachingConnectionFactory();
-        cachingConnectionFactory.setAddresses(host);
-        cachingConnectionFactory.setUsername(userName);
-        cachingConnectionFactory.setPassword(passwd);
-        cachingConnectionFactory.setVirtualHost(virtualHost);
-        cachingConnectionFactory.setPublisherConfirms(true);
-        connectionFactory = cachingConnectionFactory;
+	private Map<String, TopicExchange> topicExchangeMap = new ConcurrentHashMap<>();
+	private Map<String, Queue> queueMap = new ConcurrentHashMap<>();
+	private Map<String, AbstractMessageListenerContainer> queueListenerMap = new HashMap<>();
 
-        rabbitAdmin = new RabbitAdmin(connectionFactory);*/
-    }
+	public MQConfig() {
+		/*
+		 * CachingConnectionFactory cachingConnectionFactory = new
+		 * CachingConnectionFactory();
+		 * cachingConnectionFactory.setAddresses(host);
+		 * cachingConnectionFactory.setUsername(userName);
+		 * cachingConnectionFactory.setPassword(passwd);
+		 * cachingConnectionFactory.setVirtualHost(virtualHost);
+		 * cachingConnectionFactory.setPublisherConfirms(true);
+		 * connectionFactory = cachingConnectionFactory;
+		 * 
+		 * rabbitAdmin = new RabbitAdmin(connectionFactory);
+		 */
+	}
 
+	@Bean
+	RabbitAdmin rabbitAdmin(ConnectionFactory connectionFactory) {
+		return new RabbitAdmin(connectionFactory);
+	}
 
-    public void addNewQueueAndListener(String queueName, String exchangeName,String bindKey, MessageListener msgListener){
-        TopicExchange topicExchange = null;
-        if(topicExchangeMap.get(exchangeName) == null){
-            topicExchange = new TopicExchange(exchangeName, true, false);
-            rabbitAdmin.declareExchange(topicExchange);
-            topicExchangeMap.put(exchangeName, topicExchange);
-        }else{
-            topicExchange = topicExchangeMap.get(exchangeName);
-        }
-        Queue queue = null;
-        if(queueMap.get(queueName) == null){
-            queue = new Queue(queueName, true);
-            rabbitAdmin.declareQueue(queue);
-            queueMap.put(queueName, queue);
+	public void addNewQueueAndListener(String queueName, String exchangeName, String bindKey,
+			MessageListener msgListener) {
+		TopicExchange topicExchange = null;
+		if (topicExchangeMap.get(exchangeName) == null) {
+			topicExchange = new TopicExchange(exchangeName, true, false);
+			rabbitAdmin.declareExchange(topicExchange);
+			topicExchangeMap.put(exchangeName, topicExchange);
+		} else {
+			topicExchange = topicExchangeMap.get(exchangeName);
+		}
+		Queue queue = null;
+		if (queueMap.get(queueName) == null) {
+			queue = new Queue(queueName, true);
+			rabbitAdmin.declareQueue(queue);
+			queueMap.put(queueName, queue);
 
-            bindListener2Queue(queue, msgListener);
-        }
+			bindListener2Queue(queue, msgListener);
+		}
 
-        rabbitAdmin.declareBinding(BindingBuilder.bind(queue).to(topicExchange).with(bindKey));
-    }
+		rabbitAdmin.declareBinding(BindingBuilder.bind(queue).to(topicExchange).with(bindKey));
+	}
 
+	public void delQueueAndListener(String queueName) {
+		AbstractMessageListenerContainer listenerContainer = queueListenerMap.get(queueName);
+		queueListenerMap.remove(queueName);
+		listenerContainer.stop();
+		listenerContainer.destroy();
 
-    public void delQueueAndListener(String queueName){
-        AbstractMessageListenerContainer listenerContainer = queueListenerMap.get(queueName);
-        queueListenerMap.remove(queueName);
-        listenerContainer.stop();
-        listenerContainer.destroy();
+		Queue queue = queueMap.get(queueName);
+		queueMap.remove(queueName);
+		rabbitAdmin.deleteQueue(queueName);
+	}
 
-        Queue queue = queueMap.get(queueName);
-        queueMap.remove(queueName);
-        rabbitAdmin.deleteQueue(queueName);
-    }
+	private void bindListener2Queue(Queue queue, MessageListener msgListener) {
+		SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+		container.setConnectionFactory(connectionFactory);
+		container.setQueues(queue);
+		container.setMessageListener(msgListener);
+		container.start();
+		queueListenerMap.put(queue.getName(), container);
+	}
 
+	public Map<String, TopicExchange> getTopicExchangeMap() {
+		return topicExchangeMap;
+	}
 
-    private void bindListener2Queue(Queue queue, MessageListener msgListener){
-        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
-        container.setConnectionFactory(connectionFactory);
-        container.setQueues(queue);
-        container.setMessageListener(msgListener);
-        container.start();
-        queueListenerMap.put(queue.getName(), container);
-    }
+	public Map<String, Queue> getQueueMap() {
+		return queueMap;
+	}
 
+	public Map<String, AbstractMessageListenerContainer> getQueueListenerMap() {
+		return queueListenerMap;
+	}
 
-    public Map<String, TopicExchange> getTopicExchangeMap() {
-        return topicExchangeMap;
-    }
-    public Map<String, Queue> getQueueMap() {
-        return queueMap;
-    }
-    public Map<String, AbstractMessageListenerContainer> getQueueListenerMap() {
-        return queueListenerMap;
-    }
+	/*
+	 * public String getHost() { return host; }
+	 * 
+	 * public String getUserName() { return userName; }
+	 * 
+	 * public String getPasswd() { return passwd; }
+	 * 
+	 * public String getVirtualHost() { return virtualHost; }
+	 */
+	public String getExch() {
+		return exch;
+	}
 
- /*   public String getHost() {
-        return host;
-    }
+	public ConnectionFactory getConnectionFactory() {
+		return connectionFactory;
+	}
 
-    public String getUserName() {
-        return userName;
-    }
-
-    public String getPasswd() {
-        return passwd;
-    }
-
-    public String getVirtualHost() {
-        return virtualHost;
-    }
-*/
-    public String getExch() {
-        return exch;
-    }
-
-    public ConnectionFactory getConnectionFactory() {
-        return connectionFactory;
-    }
-
-    public RabbitAdmin getRabbitAdmin() {
-        return rabbitAdmin;
-    }
+	public RabbitAdmin getRabbitAdmin() {
+		return rabbitAdmin;
+	}
 }
-
